0d9a16014b4afae8d65a9f94472ae901616f9775
stepanusimnuel <nuel597@gmail.com>
2025-12-10 15:15:05 +0700

    load real efficientnetb0 model

--- app.py
+++ app.py
@@ -1,27 +1,51 @@
 import os
-import numpy as np
 from flask import Flask, render_template, request, send_from_directory
 from PIL import Image
-import tensorflow as tf
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-MODEL_PATH = "models/skin_model.h5"
-model = tf.keras.models.load_model(MODEL_PATH)
+MODEL_PATH = "models/classification_model_best.pth"
 
-class_names = ["normal", "oily", "dry"]
+NUM_CLASSES = 3
 
+weights = EfficientNet_B0_Weights.IMAGENET1K_V1
+model = efficientnet_b0(weights=weights)
+
+in_features = model.classifier[1].in_features
+model.classifier[1] = nn.Linear(in_features, NUM_CLASSES)
+
+state_dict = torch.load(MODEL_PATH, map_location="cpu")
+model.load_state_dict(state_dict)
+model.eval()
+
+class_names = ["dry", "normal", "oily"]
+
+# preprocess image
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
 
 def preprocess_image(image_path):
     img = Image.open(image_path).convert("RGB")
-    img = img.resize((256, 256))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
+    img = transform(img)
+    img = img.unsqueeze(0)
     return img
 
+# routes
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -37,15 +61,22 @@ def index():
         filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
         file.save(filepath)
 
-        img = preprocess_image(filepath)
-        preds = model.predict(img)
-        predicted_label = class_names[np.argmax(preds)]
+        # preprocess
+        img_tensor = preprocess_image(filepath)
+
+        # prediction
+        with torch.no_grad():
+            outputs = model(img_tensor)
+            _, predicted = torch.max(outputs, 1)
+
+        predicted_label = class_names[predicted.item()]
 
         return render_template(
             "index.html",
             preview_image=file.filename,
             prediction_result=predicted_label
         )
+
     return render_template(
         "index.html",
         preview_image=None,
@@ -56,6 +87,7 @@ def index():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
